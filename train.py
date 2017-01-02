c63421fabb3e07e21e0b7c1d2cb012da1473221d
tonyan <dongyanhit@126.com>
2017-01-02 23:27:37 +0800

    add lib files

--- train.py
+++ train.py
@@ -1,1 +1,15 @@
 #coding:utf-8
+
+import math
+import os
+import random
+import sys
+import time
+
+import numpy as np 
+from six.moves import xrange
+import tensorflow as tf 
+
+from lib import seq2seq_model
+from lib import data_utils
+
